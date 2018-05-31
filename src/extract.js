b95b236bcbd2f8c3a4356791f429fe19e9bc1bc5
yujun <yujun1@xiaomi.com>
2018-05-31 15:49:19 +0800

    feat: support export `const`.

--- src/extract.js
+++ src/extract.js
@@ -27,7 +27,7 @@ file.exists = function(filepath) {
 
 // 提取 /* public Dom__setHtml */
 function findKeyword(str) {
-    var reg = '(?:/\\* +(?:public|exported) +([^ ]*?) +\\*/|export +function +(\\w+))';
+    var reg = '(?:/\\* +(?:public|exported) +([^ ]*?) +\\*/|export +(?:function|const) +(\\w+))';
     var names = str.match(new RegExp(reg, 'g'));
     if (!names) {
         return [];
